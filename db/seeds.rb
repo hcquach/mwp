eaf8dc29613c822ee228e1d1220edb452309b346
Hien-Chanh Quach <hcquach@gmail.com>
2018-09-18 12:02:12 +0200

    First seed with parsing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'nokogiri'
+
+Product.destroy_all if Rails.env.development?
+
+batch = '173'
+product_name = 'gimme-shelter'
+url = "https://www.lewagon.com/demoday/#{batch}/#{product_name}"
+
+html_file = open(url).read
+html_doc = Nokogiri::HTML(html_file)
+
+contents = html_doc.search("meta[name='description'], meta[property='og:url'], meta[property='og:image']").map { |n|
+  n['content']
+}
+description = contents.first.strip
+url = contents.second.strip
+image = contents.third.strip
+
+new_product = Product.new(name: product_name)
+new_product.batch = batch
+new_product.description = description
+new_product.site = url
+new_product.image = image
+new_product.save
+puts "New product added"
