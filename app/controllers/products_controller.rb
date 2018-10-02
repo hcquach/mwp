7e28a73ae7795d1eeeea6a657a2c2affe4abfd8c
Hien-Chanh Quach <hcquach@gmail.com>
2018-10-02 19:08:21 +0200

    Implementing scopes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,11 @@
 class ProductsController < ApplicationController
+
   def index
-    if params[:query].present?
-      @products = Product.where("description ILIKE ?", "%#{params[:query]}%")
-    else
-      @products = Product.all
-    end
+    @products = Product.all
+    @products = @products.city(params[:city]) if params[:city].present?
+    @products = @products.year(params[:year]) if params[:year].present?
+    @products = @products.online(params[:online]) if params[:online].present?
+    @products = Product.where("description ILIKE ?", "%#{params[:query]}%") if params[:query].present?
   end
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,8 @@
 class Product < ApplicationRecord
   has_many :alumnis
+
+  scope :city, -> (city) { where city: city }
+  scope :year, -> (year) { where year: year }
+  scope :online, -> (online) { where online: online }
+
 end
