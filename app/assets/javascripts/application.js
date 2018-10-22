459bdc88eb09f96482c945d9928665ffb17fb846
Hien-Chanh Quach <hcquach@gmail.com>
2018-10-22 19:07:38 +0200

    Logo and title

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,17 +1,2 @@
 //= require rails-ujs
 //= require_tree .
-
-function infiniteScroll() {
-  const rowCards = document.querySelector('.row');
-  if (rowCards) {
-    window.addEventListener('scroll', () => {
-      more_posts_url = document.querySelector('.pagination .next a').href;
-      console.log(more_posts_url);
-    });
-  }
-}
-
-ScrollReveal().reveal('.card');
-// ScrollReveal().reveal('.card__heading', { delay: 500 });
-// ScrollReveal().reveal('.card__description', { delay: 1000 });
-// ScrollReveal().reveal('.card__subdetails', { delay: 2000 });
