43b133e435b31693d321e2157c3a250400168a70
Hien-Chanh Quach <hcquach@gmail.com>
2018-10-23 17:43:52 +0200

    Removing paginate

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,10 +2,8 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.all
-    @products = @products.city(params[:city]).paginate(:page => params[:page]) if params[:city].present?
-    @products = @products.year(params[:year]).paginate(:page => params[:page]) if params[:year].present?
-    @products = @products.online(params[:online]).paginate(:page => params[:page]) if params[:online].present?
-    @products = Product.where("description ILIKE ?", "%#{params[:query]}%").paginate(:page => params[:page]) if params[:query].present?
+    @products = @products.online(params[:online]) if params[:online].present?
+    @products = Product.where("description ILIKE ?", "%#{params[:query]}%") if params[:query].present?
 
     respond_to do |format|
       format.html
