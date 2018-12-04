4f710e0273543daa31811ba04d6e92cc0cf03a49
Hien-Chanh Quach <hcquach@gmail.com>
2018-12-04 18:32:04 +0100

    Adding most popular keywords

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,6 +6,18 @@ class ProductsController < ApplicationController
     daily_products = Daily.first.products
     @random = daily_products[-1]
 
+    frequencies = Hash.new(0)
+    all_words = ""
+    Product.all.each do |product|
+      all_words += product.description
+    end
+    words = all_words.split(/, |\s|\:|\. |\'|\.|!|-/)
+    words.each do |word|
+      frequencies[word.downcase] += 1 if !word.empty?
+    end
+    File.open(File.dirname(__FILE__) + "/../assets/config/stop_words.txt", "r").each_line { |line| frequencies.delete(line.split[0]) }
+    @frequencies = frequencies.max_by(6) { |max_value| max_value[1] }.to_h
+
     if params[:query].present?
       sql_query = "name ILIKE :query OR description ILIKE :query"
       @products = Product.where(sql_query, query: "%#{params[:query]}%").paginate(:page => params[:page]).order('id DESC')
